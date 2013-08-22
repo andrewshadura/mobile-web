7c9566d7321b1fa08edfed8646629d7fbccea7a3
Matej Simek <email@matejsimek.cz>
2013-08-22 21:11:44 +0200

    App: basic login view sending ping | #26

--- app.js
+++ app.js
@@ -13,20 +13,68 @@ var BasePanelView = BaseView.extend({
 	className: 'panel'
 });
 
+var LoginView = BasePanelView.extend({
+	attributes: {
+		title: 'ReKola - přihlášení'
+	},
+	id: 'login',
+
+	template: _.template($('#template-login').html()),
+
+	events: {
+		'click .button-login': 'doLogin'
+	},
+
+	doLogin: function(e) {
+		e.preventDefault();
+		var username = this.$('.username').val();
+		var password = this.$('.password').val();
+		$.ui.showMask('Přihlašuji...');
+		$.ajax({
+			type: 'post',
+			url: REKOLA.remoteUrl + '/accounts/mine/ping',
+			headers: {"Authorization": "Basic " + window.btoa(username + ":" + password)},
+			success: function(data) {
+				console.log('Response: ', data);
+				app.logged = true;
+				$.ui.hideMask();
+			}
+		})
+	}
+});
+
 /* Application controller */
 
 var AppController = Backbone.View.extend({
+	el: $('#content'),
+	logged: false,
+	initialize: function() {
+
+	},
+	renderLogin: function() {
+		var view = new LoginView();
+		this.$el.append(view.render().el);
+		$.ui.loadContent('#'+view.id, true, false, 'none');
+	},
 });
 
 
 var AppRouter = Backbone.Router.extend({
 	routes: {
 		'': 'index',
+		'login': 'login',
 		'*path':  'index'
 	},
 
 	index: function() {
 		console.log('router: index');
+		if(!app.logged){
+			this.go('login');
+		}
+	},
+	login: function() {
+		console.log('router: login');
+		app.renderLogin();
 	},
 
 	// Alias for navigation
