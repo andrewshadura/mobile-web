0800ec064918531f562519e489fd157df664e9cf
Matej Simek <email@matejsimek.cz>
2013-08-23 16:02:21 +0200

    Model: basic draft of Bike entity

--- app.js
+++ app.js
@@ -4,6 +4,33 @@ var app, router;
 
 /* Models */
 var Bike = Backbone.Model.extend({
+
+	defaults: function() {
+		return {
+			/*id: 0,*/
+			position: { lat: null, lng: null},
+			address: 'Ulice, č.p., Město',
+			positionNote: '',
+			type: 'old',
+			state: 'free',
+		}
+	},
+
+	borrow: function(bikeCode) {
+		this.state = 'mine';
+		// ...
+		return this._getUnlockCode(bikeCode);
+	},
+
+	putBack: function(bikePosition, note) {
+		// ...
+	},
+
+	_getUnlockCode: function(bikeCode) {
+		// ...
+		return '12345'
+	}
+
 });
 
 /* Collections */
