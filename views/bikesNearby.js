d8c72dd0627a7cb92e419df7e87f3cc5a143002b
Matej Simek <email@matejsimek.cz>
2013-08-26 17:53:51 +0200

    App: bikesNearby: better rendering | #28

--- views/bikesNearby.js
+++ views/bikesNearby.js
@@ -14,14 +14,14 @@ var BikesNearbyView = BasePanelView.extend({
 
 	initialize: function(options) {
 		console.log('BikesNearbyView init', options);
-		this.listenTo(this.model, 'all', this.render);
+		this.listenTo(this.model, 'change', this.render);
 		this.listenTo(this.model, 'destroy', this.remove);
 	},
 
 	render: function() {
 		console.log('BikesNearbyView render');
 		this.$el.html(this.template({
-			myPosition: this.options.app.userPosition.lat + ', ' + this.options.app.userPosition.lng,
+			position: this.options.app.userPosition ? this.options.app.userPosition.lat + ', ' + this.options.app.userPosition.lng : false,
 			bikes: this.model.toJSON()
 		}));
 		return this;
