f1660df3a1418ca0cbb9dc3928cf48d38f74a1c4
Matej Simek <email@matejsimek.cz>
2013-08-27 18:31:32 +0200

    App: bikeDetail: Show map with bike position and info | #29

--- views/bikeDetail.js
+++ views/bikeDetail.js
@@ -14,11 +14,37 @@ var BikeDetailView = BasePanelView.extend({
 		console.log('BikeDetailView init', options);
 	},
 
+	initMap: function() {
+		var mapEl = this.$('.map').get(0);
+		var loc = this.model.get('location');
+		var position = new google.maps.LatLng(loc.latitude, loc.longitude);
+
+		var gmap = new google.maps.Map(mapEl, {
+			center: position,
+			zoom: 17,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			mapTypeControl: false,
+			disableDefaultUI: true
+		});
+		var marker = new google.maps.Marker({
+			position: position,
+			// icon: ...,
+			map: gmap
+		});
+		// Trigger repaint on map because of render delay
+		setTimeout(function() {
+			google.maps.event.trigger(gmap, 'resize');
+			gmap.setCenter(position);
+		}, 250);
+	},
+
 	render: function() {
 		console.log('BikeDetailView render');
 		this.$el.html(this.template({
 			bike: this.model.toJSON()
 		}));
+		this.initMap();
+
 		return this;
 	},
 });
