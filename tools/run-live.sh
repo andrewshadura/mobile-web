4549f1a435e941d9063b4fc1a7b1c3cddae68b87
Tomas Susanka <tsusanka@gmail.com>
2014-03-25 11:20:13 +0100

    Deploy: no difference between live and dev build

--- tools/run-live.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#
-# Build and deploy process of the client application (ie. origin/app)
-#
-
-cd /www/rekola-mobile
-npm install
-grunt --no-color
--- tools/run.sh
+++ tools/run.sh
@@ -4,6 +4,5 @@
 # Build and deploy process of the client application (ie. origin/app)
 #
 
-cd /www/rekola-mobile-dev
 npm install
 grunt --no-color
