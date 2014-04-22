b5be2978eccb6a1152eea175ee315d664243f40f
Matej Simek <email@matejsimek.cz>
2014-04-22 17:52:32 +0200

    Conditions: works fully with API now

--- views/conditions.js
+++ views/conditions.js
@@ -14,25 +14,19 @@ var ConditionsView = BasePanelView.extend({
 	},
 	doDisagree: function(e) {
 		e.preventDefault();
-		this.sendResponse(false);
+		localStorage.removeItem(REKOLA.apiKey);
+		this.options.app.apiKey = false;
+		this.options.app.logged = false;
+		this.options.app.go('login');
 	},
 
 	sendResponse: function(agreed) {
 		var that = this;
 		that.options.app.showMask('Odesílám...');
 		that.options.app.ajax({
-			type: 'POST',
 			url: REKOLA.remoteUrl + '/accounts/mine/terms',
-			data: JSON.stringify(true),
 			success: function(result) {
-				if(agreed){
-					that.options.app.go('index');
-				} else {
-					localStorage.removeItem(REKOLA.apiKey);
-					this.options.app.apiKey = false;
-					this.options.app.logged = false;
-					this.options.app.go('login');
-				}
+				that.options.app.go('index');
 			},
 			complete: function() {
 				that.options.app.hideMask();
